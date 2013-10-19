a50ca647d5c835f1dd76a3d70f4db6357a4459c2
Benjamin Maggi <benjaminmaggi@gmail.com>
2013-10-19 15:50:00 -0300

    new named array style

--- config.js
+++ config.js
@@ -3,74 +3,66 @@
 ///////////////////////////////////////////////////////////////////////////////
 var os = require("os");
 
-///////////////////////////////////////////////////////////////////////////////
-// Development options                                                       //
-///////////////////////////////////////////////////////////////////////////////
-var development = {
-    sessionSecret: "secret",
-    environment: process.env.NODE_ENV,
-    listenPort: process.env.PORT || 8080,
-    ip: process.env.IP || os.hostname(),
-    allowCrossDomain: false,
-    autoStart: true,
-    mongoUrl: {
-        hostname: "paulo.mongohq.com",
-        port: 10026,
-        username: "admin",
-        password: "admin",
-        name: "",
-        db: "apicatus"
+var environments = {
+    ///////////////////////////////////////////////////////////////////////////
+    // Development options                                                   //
+    ///////////////////////////////////////////////////////////////////////////
+    "development": {
+        sessionSecret: "secret",
+        environment: process.env.NODE_ENV,
+        listenPort: process.env.PORT || 8080,
+        ip: process.env.IP || os.hostname(),
+        allowCrossDomain: false,
+        autoStart: true,
+        mongoUrl: {
+            hostname: "paulo.mongohq.com",
+            port: 10026,
+            username: "admin",
+            password: "admin",
+            name: "",
+            db: "apicatus"
+        }
+    },
+    ///////////////////////////////////////////////////////////////////////////
+    // Testing options                                                       //
+    // Warning: DB must be empty, do not use dev or prod databases           //
+    ///////////////////////////////////////////////////////////////////////////
+    "test": {
+        sessionSecret: "secret",
+        environment: process.env.NODE_ENV,
+        listenPort: process.env.PORT || 8080,
+        ip: process.env.IP || os.hostname(),
+        allowCrossDomain: false,
+        autoStart: false,
+        mongoUrl: {
+            hostname: "paulo.mongohq.com",
+            port: 10026,
+            username: "admin",
+            password: "admin",
+            name: "",
+            db: "apicatus"
+        }
+    },
+    ///////////////////////////////////////////////////////////////////////////
+    // Production options                                                    //
+    ///////////////////////////////////////////////////////////////////////////
+    production: {
+        sessionSecret: "secret",
+        environment: process.env.NODE_ENV,
+        listenPort: process.env.PORT || 8080,
+        ip: process.env.IP || os.hostname(),
+        allowCrossDomain: false,
+        autoStart: true,
+        mongoUrl: {
+            hostname: "paulo.mongohq.com",
+            port: 10026,
+            username: "admin",
+            password: "admin",
+            name: "",
+            db: "apicatus"
+        }
     }
-};
-///////////////////////////////////////////////////////////////////////////////
-// Testing options                                                           //
-// Warning: DB must be empty, do not use dev or prod databases               //
-///////////////////////////////////////////////////////////////////////////////
-var test = {
-    sessionSecret: "secret",
-    environment: process.env.NODE_ENV,
-    listenPort: process.env.PORT || 8080,
-    ip: process.env.IP || os.hostname(),
-    allowCrossDomain: false,
-    autoStart: false,
-    mongoUrl: {
-        hostname: "paulo.mongohq.com",
-        port: 10026,
-        username: "admin",
-        password: "admin",
-        name: "",
-        db: "apicatus"
-    }
-};
-///////////////////////////////////////////////////////////////////////////////
-// Production options                                                        //
-///////////////////////////////////////////////////////////////////////////////
-var production = {
-    sessionSecret: "secret",
-    environment: process.env.NODE_ENV,
-    listenPort: process.env.PORT || 8080,
-    ip: process.env.IP || os.hostname(),
-    allowCrossDomain: false,
-    autoStart: true,
-    mongoUrl: {
-        hostname: "paulo.mongohq.com",
-        port: 10026,
-        username: "admin",
-        password: "admin",
-        name: "",
-        db: "apicatus"
-    }
-};
-
+}
 module.exports = (function(){
-    switch(process.env.NODE_ENV){
-        case 'development':
-            return development;
-        case 'test':
-            return test;
-        case 'production':
-            return production;
-        default:
-            return development;
-    }
+    return environments[process.env.NODE_ENV];
 })();

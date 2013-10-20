a9779d16a270749c737c7443198c1c0162ff9ff2
Benjamin Maggi <benjaminmaggi@gmail.com>
2013-10-20 16:19:54 -0400

    remove bcrypt

--- models/account.js
+++ models/account.js
@@ -1,5 +1,4 @@
-var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt'),
+var mongoose = require('mongoose')
     SALT_WORK_FACTOR = 10,
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
@@ -13,45 +12,6 @@ var Account = new Schema({
     digestors: { type: Array, required: false },
 });
 
-
-// Remember Me implementation helper method
-Account.methods.generateRandomToken = function () {
-    var user = this,
-        chars = "_!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        token = new Date().getTime() + '_';
-
-    for ( var x = 0; x < 16; x++ ) {
-        var i = Math.floor( Math.random() * 62 );
-        token += chars.charAt( i );
-    }
-    return token;
-};
-
-// Bcrypt middleware
-/*
-Account.pre('save', function(next) {
-    var user = this;
-    if(!user.isModified('password')) return next();
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if(err) return next(err);
-
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if(err) return next(err);
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-// Password verification
-Account.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if(err) return cb(err);
-        cb(null, isMatch);
-    });
-};
-
-*/
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);

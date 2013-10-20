78da8a31c7bd1d197dc59becd23db6873df6afb3
Benjamin Maggi <benjaminmaggi@gmail.com>
2013-10-20 16:28:49 -0300

    adds correct bcrypt version

--- models/account.js
+++ models/account.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
-	bcrypt = require('bcrypt'),
-	SALT_WORK_FACTOR = 10,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10,
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
 
@@ -16,39 +16,39 @@ var Account = new Schema({
 
 // Remember Me implementation helper method
 Account.methods.generateRandomToken = function () {
-	var user = this,
-		chars = "_!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-		token = new Date().getTime() + '_';
+    var user = this,
+        chars = "_!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        token = new Date().getTime() + '_';
 
-	for ( var x = 0; x < 16; x++ ) {
-		var i = Math.floor( Math.random() * 62 );
-		token += chars.charAt( i );
-	}
-	return token;
+    for ( var x = 0; x < 16; x++ ) {
+        var i = Math.floor( Math.random() * 62 );
+        token += chars.charAt( i );
+    }
+    return token;
 };
 
 // Bcrypt middleware
 /*
 Account.pre('save', function(next) {
-	var user = this;
-	if(!user.isModified('password')) return next();
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if(err) return next(err);
+    var user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if(err) return next(err);
 
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			if(err) return next(err);
-			user.password = hash;
-			next();
-		});
-	});
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 // Password verification
 Account.methods.comparePassword = function(candidatePassword, cb) {
-	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-		if(err) return cb(err);
-		cb(null, isMatch);
-	});
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
 };
 
 */
